34a37954c18d238e2e5d0c79edf958863d80c947
Ahmad Rashidh <ahmadrashidh@Ahmads-MacBook-Pro.local>
2025-06-27 16:35:29 +0100

    locally ran, PUSHING FOR TFC

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
 
-provider "aws" {
-  region = var.region
+  required_version = ">= 1.2.0"
 }
 
 
-resource "aws_instance" "ubuntu_free_tier" {
-  ami           = "ami-05ffe3c48a9991133" // amazon linux free tier provision
-  instance_type = "t2.micro"
+output "instance_ami" {
+  value = "Hello World!"
 }
+

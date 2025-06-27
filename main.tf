1060a56753c034be73e33d11f88e54b253f8e427
Ahmad Rashidh <ahmadrashidh@Ahmads-MacBook-Pro.local>
2025-06-27 11:48:31 +0100

    error fixed

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "cldy-tf-pg" {
-  ami           = "ami-05ffe3c48a9991133"  # DUMMY AMI ID
+resource "aws_instance" "ubuntu_free_tier" {
+  ami           = "ami-05ffe3c48a9991133" // amazon linux free tier provision
   instance_type = "t2.micro"
 }
